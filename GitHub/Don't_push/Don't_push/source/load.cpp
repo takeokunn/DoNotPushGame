1bcb66932674da7f18e9d98ba5baab1b349fd6f6
yumetodo <yume-wikijp@live.jp>
2015-08-27 16:06:41 +0900

    make_sound_array関数を作成。

--- GitHub/Don't_push/Don't_push/source/load.cpp
+++ GitHub/Don't_push/Don't_push/source/load.cpp
@@ -10,11 +10,27 @@ private:
 	std::string path;
 };
 std::unordered_map<std::string, DxGHandle> make_image_array(const std::string& path){
-
 	std::unordered_map<std::string, DxGHandle> rtn;
 	make_DxGHandle_pair img((path.back() == '\\') ? path : path + '\\');
 	rtn.insert(img("title"));
 	rtn.insert(img("game"));
 	rtn.insert(img("end"));
 	return rtn;
+}
+class make_DxSHandle_pair {
+public:
+	make_DxSHandle_pair(const std::string& path_) : path(path_) {}
+	make_DxSHandle_pair(std::string&& path_) : path(path_) {}
+	std::pair<std::string, DxSHandle> operator() (const std::string& name, const char* ext = "wav") {
+		return make_pair(name, DxSHandle(this->path + name + "." + ext));
+	}
+private:
+	std::string path;
+};
+std::unordered_map<std::string, DxSHandle> make_sound_array(const std::string& path) {
+	std::unordered_map<std::string, DxSHandle> re;
+	make_DxSHandle_pair sound((path.back() == '\\') ? path : path + '\\');
+	re.insert(sound("flower garden"));
+	re.insert(sound("title"));
+	return re;
 }
--- GitHub/Don't_push/Don't_push/source/load.h
+++ GitHub/Don't_push/Don't_push/source/load.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 #include "DxGraphicHandle.h"
+#include "DxSoundHandle.h"
 #include <unordered_map>
 #include <string>
 std::unordered_map<std::string, DxGHandle> make_image_array(const std::string& path);
+std::unordered_map<std::string, DxSHandle> make_sound_array(const std::string& path);
