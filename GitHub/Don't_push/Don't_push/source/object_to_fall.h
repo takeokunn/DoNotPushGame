474e31654ecf3c627d2a5a2f27eb392f90063bba
yumetodo <yume-wikijp@live.jp>
2015-08-27 14:50:03 +0900

    object_to_fall_cクラスを作成開始

--- GitHub/Don't_push/Don't_push/source/object_to_fall.h
+++ GitHub/Don't_push/Don't_push/source/object_to_fall.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "define.h"
+#include "DxGraphicHandle.h"
+class object_to_fall_c
+{
+public:
+	object_to_fall_c(int x, int y, const object_to_fall_c& handle);
+	object_to_fall_c(INT2_t p, const object_to_fall_c& handle);
+	INT2_t xy_move();
+private:
+	int x_;
+	int y_;
+	DxGHandle handle_;
+};
