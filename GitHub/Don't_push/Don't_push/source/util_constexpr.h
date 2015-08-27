c67b3934a8013c07143aa434ee254d859fb09c62
yumetodo <yume-wikijp@live.jp>
2015-08-27 13:56:09 +0900

    constexpr対応

--- GitHub/Don't_push/Don't_push/source/util_constexpr.h
+++ GitHub/Don't_push/Don't_push/source/util_constexpr.h
@@ -1,5 +1,6 @@
 #pragma once
 #if (defined(_MSC_VER) && _MSC_FULL_VER >= 180031101) ||(defined(__clang__) && (__clang_major__ > 3 || (__clang_major__ == 3 && __clang_minor__ >= 4))) || (defined(__GNUC__) && (__GNUC__ > 4  || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)))
+#define SUPPORT_CONSTEXPR 1
 #define CONSTEXPR constexpr
 #define CONSTEXPR_OR_CONST constexpr
 #define CONSTEXPR_OR_STATICCONST constexpr
