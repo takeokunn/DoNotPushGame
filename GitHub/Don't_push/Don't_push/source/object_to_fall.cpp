3ca36ce7bba942f25cd3b03070a8fc2083002760
yumetodo <yume-wikijp@live.jp>
2015-08-27 17:34:42 +0900

    object_to_fall_cのコンストラクタのバグを修正、パワーバークラス用ファイル追加

--- GitHub/Don't_push/Don't_push/source/object_to_fall.cpp
+++ GitHub/Don't_push/Don't_push/source/object_to_fall.cpp
@@ -1,8 +1,8 @@
 #include "object_to_fall.h"
 
 object_to_fall_c::object_to_fall_c(const object_to_fall_c & o) : handle_(o.handle_), self_(o.self_), pixcel_per_frame_(o.pixcel_per_frame_){}
-object_to_fall_c::object_to_fall_c(int x, int y, int pixcel_per_frame, const DxGHandle & handle) : handle_(handle), self_({x, y}), pixcel_per_frame_(pixcel_per_frame_) {}
-object_to_fall_c::object_to_fall_c(INT2_t p, int pixcel_per_frame, const DxGHandle & handle) : handle_(handle), self_(p), pixcel_per_frame_(pixcel_per_frame_) {}
+object_to_fall_c::object_to_fall_c(int x, int y, int pixcel_per_frame, const DxGHandle & handle) : handle_(handle), self_({x, y}), pixcel_per_frame_(pixcel_per_frame) {}
+object_to_fall_c::object_to_fall_c(INT2_t p, int pixcel_per_frame, const DxGHandle & handle) : handle_(handle), self_(p), pixcel_per_frame_(pixcel_per_frame) {}
 
 INT2_t object_to_fall_c::xy_move(){
 	return INT2_t({this->self_.first, this->self_.second + this->pixcel_per_frame_});
