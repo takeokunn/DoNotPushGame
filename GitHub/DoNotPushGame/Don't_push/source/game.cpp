add97b0b7b66e5075eab523e914855cdcd740c4c
siryumsr <knviryums@yahoo.co.jp>
2015-08-27 11:59:15 +0900

    キャラクターの移動

--- GitHub/DoNotPushGame/Don't_push/source/game.cpp
+++ GitHub/DoNotPushGame/Don't_push/source/game.cpp
@@ -8,18 +8,27 @@ void foo(){
 	if (state.left())  x -= 1;
 	if (state != KEY_INPUT_PGUP) x += 2;
 }
-int ver(int current_x, keystate& state){
+int move_x(int current_x, keystate& state){
 	constexpr int CHARACTER_MOVE_SPEED = 4;
 	int re = current_x;
 	state.update();
-	if (state.left() || state == KEY_INPUT_A) re -= CHARACTER_MOVE_SPEED;
-
+	if (state.left() ) re -= CHARACTER_MOVE_SPEED;
+	if (state.right()) re += CHARACTER_MOVE_SPEED;
+	return re;
 }
 
 Status game(img_arr_t& img_arr){
 	ClearDrawScreen();
 	img_arr["game"].DrawGraph(0, 0, false);
 	ScreenFlip();
+	keystate state;
+	int x;
+	while (1){
+		x = move_x(x, state);
+		img_arr["game"].DrawGraph(x, 0, false);
+		ScreenFlip();
+		ClearDrawScreen();
+	}
 
 	while (CheckHitKey(KEY_INPUT_D) != 1);
 	return Status::END;
