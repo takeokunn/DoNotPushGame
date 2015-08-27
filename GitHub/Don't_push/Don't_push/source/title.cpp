6f7ccfecd90001d6d962792cbc67b18822443fbb
yuchu7 <yuchu222@yahoo.co.jp>
2015-08-27 15:38:59 +0900

    title change

--- GitHub/Don't_push/Don't_push/source/title.cpp
+++ GitHub/Don't_push/Don't_push/source/title.cpp
@@ -1,19 +1,47 @@
 ﻿#include "title.h"
 #include "DxLib.h"
 Status title(img_arr_t& img_arr) {
-	ClearDrawScreen();
-	img_arr["title"].DrawGraph(0, 0, false);
-	ScreenFlip();
+	
+	//img_arr["title"].DrawGraph(0, 0, false);
 
 	/////////////
 
-	int Font_title;
-	Font_title = CreateFontToHandle(NULL, 20, 5, DX_FONTTYPE_EDGE);
+	//画像の読み込み
+	
 
+	//カウンター
+	int count = 0;
+
+	//フォントの定義
+	int Font_title;		//タイトルロゴ
+	int Font_1;			//「Zキーを押してね」の奴
+	Font_title = CreateFontToHandle(NULL, 70, 5, DX_FONTTYPE_EDGE);
+	Font_1 = CreateFontToHandle(NULL, 30, 1, DX_FONTTYPE_ANTIALIASING);
+
+	while (TRUE){
+		count++;
+		ClearDrawScreen();
+
+		//描画
+		DrawBox(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, GetColor(100, 180, 250), TRUE);	//背景
+
+		//img_arr["red2"].DrawGraph(30, 10, TRUE);
+
+		DrawStringToHandle(30 + 5, WINDOW_HEIGHT / 3 + 5, "D...DON'T PUSH!!", GetColor(255, 255, 255), Font_title);		//タイトル（影）
+		DrawStringToHandle(30, WINDOW_HEIGHT / 3, "D...DON'T PUSH!!", GetColor(0, 0, 0), Font_title);					//タイトル
+		DrawStringToHandle(180, WINDOW_HEIGHT - (WINDOW_HEIGHT / 3), "- Zキーを押してね -", GetColor(0, 0, 0), Font_1);//Zキー押してね
+
+		ScreenFlip();
+
+		if (CheckHitKey(KEY_INPUT_Z) == 1){
+			break;
+		}
+	}
+
+	
 
 	///////////// by yuchu
 
-	while (CheckHitKey(KEY_INPUT_Z) != 1);
 	return Status::GAME;
 
 }
