32744b6c9a26e12a65ed14c684f1665dd23d375d
yumetodo <yume-wikijp@live.jp>
2016-02-09 13:10:30 +0900

    add config.cpp

---
+++ Don't_push/Don't_push/source/config.cpp
@@ -0,0 +1,6 @@
+﻿#include "config.h"
+
+config_info load_config(const std::string & config_json_path)
+{
+	return config_info();
+}
--- Don't_push/Don't_push/source/config.h
+++ Don't_push/Don't_push/source/config.h
@@ -2,5 +2,14 @@
 #include <unordered_map>
 #include <string>
 struct config_info {
-
+	config_info() = default;
+	config_info(const config_info&) = default;
+	config_info(config_info&& o) : lang_str(o.lang_str) {}
+	config_info& operator=(const config_info&) = default;
+	config_info& operator=(config_info&& o) {
+		this->lang_str = std::move(o.lang_str);
+	}
+	std::unordered_map<std::wstring, std::wstring> lang_str;
 };
+
+config_info load_config(const std::string& config_json_path);
